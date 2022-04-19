e3f267b6ee99fd96122e24cb415a1919df43a33d
Cercadu <Cadu-ce1@hotmail.com>
2022-04-18 23:41:28 -0300

    Inclusao de exercícios - Primeiros passos para desenvolvimento web

---
+++ primeiros_passos_para_desenvolvimento_web/pagina.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Desafio Criando Acordion JQuery</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Seletor 1</h3>
+  <div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui oficia deserunt 
+        mollit anim id est laborum.
+    </p>
+  </div>
+  <h3>Seletor 2</h3>
+  <div>
+    <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque 
+        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi 
+        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+        sit aspernatur aut odit aut fugit, sed quia consecuntur magni dolores eos qui ratione 
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit 
+        amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut 
+        labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam , quis 
+        nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
+        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam 
+        nihil molestiae consequatur,vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+    </p>
+  </div>
+  <h3>Seletor 3</h3>
+  <div>
+    <p>
+        exceto para obter alguma vantagem com isso? Mas quem tem o direito de criticar um homem 
+        que escolhe desfrutar de um prazer que não tem consequências irritantes, ou aquele que 
+        evita uma dor que não produz nenhum prazer?
+    </p>
+ </div>
+  <h3>Seletor 4</h3>
+  <div>
+    <p>
+        Cras dictum. Pellentesque habitant morbi tristique senectus et netus
+        et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
+        mauris vel est.
+    </p>
+    <p>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium 
+        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati 
+        cupiditate non provident, similique sunt in culpa qui oficia deserunt mollitia animi, id est
+         laborum et dolorum fuga. rerum facilis est et expedita distinctio. Nam libero tempore, cum 
+         soluta nobis est eligendi optio cumque nihil impedit quo menos id quod maxime placeat facere 
+         possimus, omnis voluptas assuminda est, omnis dolor repellendus. Temporibus autem quibusdam
+         officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae
+         sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
+         aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores
+         repellat.
+    </p>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
